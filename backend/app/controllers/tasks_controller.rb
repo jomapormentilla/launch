ecd4ed864b55c75e536e7e77b8e5d52a29060f44
jomapormentilla <jomapormentilla@gmail.com>
2021-02-13 15:04:58 -0500

    Update Task Controller actions.

--- backend/app/controllers/tasks_controller.rb
+++ backend/app/controllers/tasks_controller.rb
@@ -3,4 +3,51 @@ class TasksController < ApplicationController
         tasks = Task.all.includes(:user, :project)
         render json: TaskSerializer.new(tasks).to_serialized_json
     end
+
+    def create
+        task = Task.new(task_params)
+
+        if task.save
+            render json: TaskSerializer(task).to_serialized_json
+        else
+            render json: { error: 'Something went wrong! Unable to create Task' }
+        end
+    end
+
+    def show
+        task = Task.find_by_id(params[:id])
+
+        if task
+            render json: TaskSerializer.new(task).to_serialized_json
+        else
+            render json: { error: 'Unable to find Task' }
+        end
+    end
+
+    def update
+        task = Task.find_by_id(params[:id])
+
+        if task.update
+            render json: TaskSerializer.new(task).to_serialized_json
+        else
+            render json: { error: 'Unable to update Task' }
+        end
+    end
+
+    def destroy
+        task = Task.find_by_id(params[:id])
+
+        if task
+            task.destroy
+            render json: { message: 'Task successfully deleted.' }
+        else
+            render json: { error: 'Unable to delete Task.' }
+        end
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :description, :deadline, :user_id, :project_id, :status)
+    end
 end
