6e9ac84aeeb7e1d29c95bd6a5e2084b1c3d3a27f
jomapormentilla <jomapormentilla@gmail.com>
2021-02-13 14:56:42 -0500

    Build out backend User Controller actions.

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -3,10 +3,41 @@ class UsersController < ApplicationController
         users = User.all.includes(:projects, :tasks, :department)
         render json: UserSerializer.new(users).to_serialized_json
     end
+
+    def create
+        user = User.new(user_params)
+    
+        if user.save
+            render json: UserSerializer.new(user).to_serialized_json
+        else
+            render json: { error: 'Something went wrong: User could not be saved.' }
+        end
+    end
     
     def show
         user = User.find_by(id: params[:id])
         render json: UserSerializer.new(user).to_serialized_json
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+
+        if user.update(user_params)
+            render json: UserSerializer.new(user).to_serialized_json
+        else
+            render json: { error: 'Something went wrong: User could not be updated.' }
+        end
+    end
+
+    def destroy
+        user = User.find_by_id(params[:id])
+
+        if user
+            user.destroy
+            render json: { message: 'User successfully deleted from database.' }
+        else
+            render json: { error: 'Unable to delete User' }
+        end
     end
 
     private
