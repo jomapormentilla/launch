e6b95c1c5fe7108709db0b7d92f4ec63b0f7f4fe
jomapormentilla <jomapormentilla@gmail.com>
2021-02-18 12:26:16 -0500

    Build create method for Message controller.

--- backend/app/controllers/messages_controller.rb
+++ backend/app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+    def create
+        message = Message.new(message_params)
+
+        if message.save
+            render json: message
+        else
+            render json: { error: 'Something went wrong! Unable to save message.' }
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:content, :sender_id, :receiver_id)
+    end
 end
