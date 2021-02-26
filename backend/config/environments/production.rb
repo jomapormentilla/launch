b96f85ea224bc3e6728ab2feb9acd670baf171de
jomapormentilla <jomapormentilla@gmail.com>
2021-02-26 13:35:54 -0500

    Revert websocket backend changes.

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -110,8 +110,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  # Web Sockets on Heroku
-  config.middleware.use MessageChannel
-  config.web_socket_server_url = "ws://launch-it.herokuapp.com/"
 end
