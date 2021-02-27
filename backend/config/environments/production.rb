b5aff81bd22cad0d6c577468f9cfbcd0c82cb99d
jomapormentilla <jomapormentilla@gmail.com>
2021-02-27 17:11:31 -0500

    Fix CORS

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://launch-it.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://launch-it.netlify.app/' ]
+  config.action_cable.allowed_request_origins = [ 'https://launch-it.netlify.app' ]
   # config.action_cable.allowed_request_origins = [ 'https://launch-it.netlify.app/', /http:\/\/launch-it.netlify.app.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://launch-it.netlify.app/'
+    origins 'https://launch-it.netlify.app'
 
     resource '*',
       headers: :any,
